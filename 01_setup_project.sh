9a0807254ffe37678e8c4db02ee1fde3c1aac7f6
Kailie Dela Cruz <kmdelacruz@dons.usfca.edu>
2018-09-14 16:35:32 -0700

    Create directory and file structure

---
+++ 01_setup_project.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+
+# Script to create a directory and file structure
+# Initially created by Kailie Dela Cruz
+# September 14, 2018
+# kmdelacruz@dons.usfca.edu
+
+# Set up directory structure
+echo "Setting up directory structure"
+echo "code data data/raw_data output output/figures output/tables"
+mkdir code data data/raw_data output output/figures output/tables
+
+# Download the sequence data
+echo "Downloading sequence data"
+curl -L http://npk.io/PUlBaY+ -o fasta_archive.zip
+unzip fasta_archive.zip -d data/raw_data/
+
+# Remove unzipped fasta file from directory
+echo "Removing unzipped fasta file"
+rm fasta_archive.zip 
