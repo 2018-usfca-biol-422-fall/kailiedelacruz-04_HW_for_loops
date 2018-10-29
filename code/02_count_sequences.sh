5e1e7869cd32dc62f8f4e7c089a33f8177628a13
Kailie Dela Cruz <kmdelacruz@dons.usfca.edu>
2018-10-28 23:23:04 -0700

    Edit for loop to read multiple files

--- code/02_count_sequences.sh
+++ code/02_count_sequences.sh
@@ -1,15 +1,20 @@
-#!sh/bash/dash/ksh
+#!sh/#!/usr/bin/env bash
 
 # Script to create a directory and file structure
-# Initially created by Kailie Dela Cruz
+# Initially created by Kailie Dela kmdelacruz
 # September 14, 2018
 # kmdelacruz@dons.usfca.edu
 
-# Count the number of sequences in fasta files
-echo "Counting the number of sequences in fasta files"
+# Count the number of sequences in multiple fasta files
+echo "Counting the number of sequences in multiple fasta files"
 for file in "$@"
 do
   FILENAME=$(basename "$file" .fasta)
-  COUNT=$(grep -c ">" "$file")
+  COUNT=$(grep ">" "$file" -c | tr -d " ")
+  echo The file "$FILENAME" has "$COUNT" sequences in it
 done
-  echo In "$FILENAME", there are "$COUNT" sequences.
+
+# To count the sequences in one file here is an example
+# grep ">" data/raw_data/animal_mRNA.fasta | wc -l
+# A for loop must be used in order to count the sequences
+# in multiple fasta files
